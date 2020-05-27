cc605dfd9171100b8387f24695cad85891638f71
Thomas Pintaric <thomas@pintaric.org>
2020-05-27 10:25:36 +0200

    Added missing file(s), cherry-picked changes from curve fitting branch.

--- ros_packages/group3_lab7/include/motion_planner/path_interpolation.h
+++ ros_packages/group3_lab7/include/motion_planner/path_interpolation.h
@@ -10,9 +10,51 @@
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 
-float LinearCurveLength(const std::deque<Eigen::Vector2f> &curve);
+template<typename T>
+T LinearCurveLength(std::deque<Eigen::Matrix<T, 2, 1> > const &curve) {
+    T sum = 0.0;
+    for(unsigned int j=1; j < curve.size(); j++) {
+        sum += (curve[j]-curve[j-1]).norm();
+    }
+    return(sum);
+}
 
-std::deque<Eigen::Vector2f> UniformLinearInterpolation(
-        const std::deque<Eigen::Vector2f> &curve,
-        std::size_t number_of_points,
-        const float known_linear_curve_length = 0.0f);
+template<typename T, typename U>
+std::vector<Eigen::Matrix<T, Eigen::Dynamic, 1> > UniformLinearInterpolation(
+        const std::deque<Eigen::Matrix<U, 2, 1> > &curve,
+        std::size_t target_size,
+        const T known_linear_curve_length = 0) {
+
+    std::vector<Eigen::Matrix<T, Eigen::Dynamic, 1> > interpolated_curve;
+    if(curve.size() < 2 || target_size < 2) {
+        // degenerate input curve or invalid target size
+        return interpolated_curve;
+    }
+    const T total_length = (known_linear_curve_length > 0.0f) ?
+            known_linear_curve_length : LinearCurveLength<U>(curve);
+
+    const T segment_length = total_length / (T)(target_size - 1);
+
+    auto start = 0;
+    auto finish = start + 1;
+    T src_segment_offset = 0.0f;
+    T src_segment_length =  (curve[start] - curve[finish]).norm();
+
+    interpolated_curve.push_back(curve[start].template cast<T>());
+
+    for(size_t i=1; i<target_size-1; i++) {
+        const T next_offset = segment_length * i;
+        while(src_segment_offset + src_segment_length < next_offset) {
+            src_segment_offset += src_segment_length;
+            start = finish++;
+            src_segment_length = (curve[start] - curve[finish]).norm();
+        }
+        const T part_offset = next_offset - src_segment_offset;
+        const T part_ratio = part_offset / src_segment_length;
+        interpolated_curve.push_back(Eigen::Matrix<T, 2, 1>(
+                curve[start](0) +  part_ratio * (curve[finish](0) - curve[start](0)),
+                curve[start](1) +  part_ratio * (curve[finish](1) - curve[start](1))));
+    }
+    interpolated_curve.push_back(curve.back().template cast<T>());
+    return interpolated_curve;
+}
--- ros_packages/group3_lab7/src/motion_planner/path_follower.cpp
+++ ros_packages/group3_lab7/src/motion_planner/path_follower.cpp
@@ -26,26 +26,39 @@ PathFollower::PathFollower(Options options) : options_(options) {}
 std::tuple<bool, PathFollower::Path>
 PathFollower::refinePath(const Path& path) {
 
-    const unsigned int number_of_points = std::max<unsigned int>(5, options_.cubic_spline_points_); // need at least 5 points
-    const auto refined_path = UniformLinearInterpolation(path, number_of_points);
-
-    if(refined_path.size() == 0) {
+    if(path.size() < 2) {
         Path empty_path;
         return {false, empty_path};
     }
 
-    std::vector<float> x(refined_path.size());
-    std::vector<float> y(refined_path.size());
-    for(unsigned int i=0; i<refined_path.size(); i++) {
-        x[i] = refined_path[i](0);
-        y[i] = refined_path[i](1);
+    const float linear_path_length = LinearCurveLength<float>(path);
+
+    constexpr unsigned int minimum_number_of_points_required = 5;
+    //const unsigned int number_of_points_on_resampled_path = std::max<unsigned int>(
+    //        (unsigned int) std::ceil(linear_path_length / options_.path_segment_length_),
+    //        minimum_number_of_points_required);
+
+    const unsigned int number_of_points_on_resampled_path = std::max<unsigned int>(
+            options_.cubic_spline_points_,
+            minimum_number_of_points_required);
+
+    const std::vector<Eigen::VectorXf> resampled_path = UniformLinearInterpolation<float, float>(
+            path, number_of_points_on_resampled_path, linear_path_length);
+
+    assert(resampled_path.size() >= minimum_number_of_points_required);
+
+    std::vector<float> x(resampled_path.size());
+    std::vector<float> y(resampled_path.size());
+    for(unsigned int i=0; i<resampled_path.size(); i++) {
+        x[i] = resampled_path[i](0);
+        y[i] = resampled_path[i](1);
     }
 
     const float t_step = 1.0 / (float) (options_.interpolated_points_on_path_-1);
     std::vector<float> t(options_.interpolated_points_on_path_);
     std::generate(t.begin(), t.end(), [n = 0, &t_step]() mutable { return (float)(n++) * t_step; });
 
-    const float step = 1.0 / (float) (refined_path.size()-1);
+    const float step = 1.0 / (float) (resampled_path.size()-1);
     boost::math::cubic_b_spline<float> spline_x(x.data(), x.size(), 0.0f /* start time */, step);
     boost::math::cubic_b_spline<float> spline_y(y.data(), y.size(), 0.0f /* start time */, step);
 
