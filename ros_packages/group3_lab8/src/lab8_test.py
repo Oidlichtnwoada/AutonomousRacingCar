dde1aab03d040b7a47091725c52833fe55c6b8fe
Hannes Brantner <e01614466@student.tuwien.ac.at>
2020-06-16 08:44:31 +0200

    changed reward function, close in check_done is still not computed right and reformatted code

--- ros_packages/group3_lab8/src/lab8_test.py
+++ ros_packages/group3_lab8/src/lab8_test.py
@@ -3,18 +3,20 @@
 
 from stable_baselines import PPO2
 
-#ROS Imports
+# ROS Imports
 import rospy
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
 import sys
 import numpy as np
 
+
 class WallFollow:
     """ Implement Wall Following on the car
     """
+
     def __init__(self):
-        self.trained_model = PPO2.load("deepqn_lab8") # <-- adjust path
+        self.trained_model = PPO2.load("deepqn_lab8")  # <-- adjust path
         self.lidar_sub = rospy.Subscriber('scan', LaserScan, self.lidar_callback, queue_size=1)
         self.drive_pub = rospy.Publisher('nav', AckermannDriveStamped, queue_size=1)
 
@@ -26,12 +28,13 @@ class WallFollow:
         drive_msg.header.frame_id = "laser"
         drive_msg.drive.steering_angle = actions[0][1]
         drive_msg.drive.speed = actions[0][0]
-        
+
         # publish data and catch possible "Topic-already-closed"-Exception when ROS is shutdown --> exit this node
         try:
             self.drive_pub.publish(drive_msg)
         except rospy.ROSException:
             exit(0)
+
 
 def main(args):
     rospy.init_node("WallFollow_node", anonymous=True)
@@ -39,5 +42,6 @@ def main(args):
     rospy.sleep(0.1)
     rospy.spin()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main(sys.argv)
